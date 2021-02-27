08c312f2ab4c88ee1dcbf142b3d5c98a87d5bb02
Prakash Wagle <prakashwagle@live.in>
2021-02-27 16:45:03 -0500

    Initialize project CryptoTracker

---
+++ crypto-ticker/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="App">
+        <p>
+           <code>src/App.js</code> and save to reload.
+        </p>
+<h1>Crypto Ticker</h1>
+    </div>
+  );
+}
+
+export default App;
