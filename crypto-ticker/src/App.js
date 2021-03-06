686237ef9fa057885c4bc7620f8137dcf7246d3b
Prakash Wagle <prakashwagle@live.in>
2021-03-06 17:34:04 -0500

    feat: created a new component for Price Display

--- crypto-ticker/src/App.js
+++ crypto-ticker/src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import PriceCard from './components/PriceCard';
 import Logo from "./logo/bitcoin.png"
 
 const App = () => {
@@ -7,6 +8,11 @@ const App = () => {
     <img src={Logo} width={250} height={150} alt="Crypto Logo" />
     <h1 className="title">Live Crypto Price</h1>
     <h5 className="subtitle">Bitcoin To The Moon 🚀🌕</h5>
+    <div className="prices-container">
+      <PriceCard type = "low" price={100}/>
+      <PriceCard type = "high" price={400}/>
+      <PriceCard type = "current" price={300}/>
+      </div>
   </div>
   );
 }
